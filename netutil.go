36bbb63d4f42e78fa60cad94a47ea7c8f301c1a8
Jesse Lucas <jesse@forestgiant.com>
2016-04-18 11:39:14 -0400

    Starting netutil package

---
+++ netutil.go
@@ -0,0 +1,54 @@
+package netutil
+
+import (
+	"errors"
+	"net"
+)
+
+// IsLocalhost takes an address and checks to see if
+// it matches the local computers
+func IsLocalhost(target string) bool {
+	ip := net.ParseIP(target)
+
+	addrs, _ := net.InterfaceAddrs()
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok {
+			if ip.Equal(ipnet.IP) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// ExternalIP return the network address of the computer
+func ExternalIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, address := range addrs {
+		// check the address type and make sure it's not loopback
+		if ipnet, ok := address.(*net.IPNet); ok {
+			if !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					return ipnet.IP.To4().String(), nil
+				}
+			}
+		}
+	}
+
+	// couldn't get external, send loopback address
+	for _, address := range addrs {
+		// check the address type and make sure it's not loopback
+		if ipnet, ok := address.(*net.IPNet); ok {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.To4().String(), nil
+			}
+		}
+	}
+
+	return "", errors.New("Couldn't get external IP")
+}
